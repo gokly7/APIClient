e30ea2e4439e4eeef0c137ba0409ac1417b5ae43
Ayuso <gokly500@gmail.com>
2020-05-19 13:32:19 +0200

    test

---
+++ src/Cliente.php
@@ -0,0 +1,26 @@
+<?php
+include __DIR__ . "/../vendor/autoload.php";
+include 'DBconn.php';
+
+class Cliente extends DBconn{
+
+    function insertarClientes($name,$address,$city,$email,$date,$quantity){
+        $result = $this->connect()->query("INSERT INTO cliente(name,address,city,email,date,quantity) VALUES ('$name','$address','$city','$email','$date',$quantity)");
+        return $result;
+    }
+    function getClientes(){
+        $result = $this->connect()->query('SELECT * FROM cliente');
+        return $result;
+    }
+    function getCliente($name){
+        $result = $this->connect()->query("SELECT * FROM cliente WHERE name LIKE '%$name%'");
+        return $result;
+    }
+    function getClientesDate($date){
+        $result = $this->connect()->query("SELECT * FROM cliente WHERE date > '$date'");
+        return $result;
+    }
+    
+}
+
+?>
